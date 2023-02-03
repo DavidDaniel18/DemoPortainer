d9cd17c92149e8720150f5ec52a00463271b149a
DavidDaniel18 <david.vermette-nadeau.1@ens.etsmtl.ca>
2023-02-03 14:19:19 -0500

    update

--- PortainerTest/PortainerTest/Program.cs
+++ PortainerTest/PortainerTest/Program.cs
@@ -15,12 +15,8 @@ namespace PortainerTest
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
